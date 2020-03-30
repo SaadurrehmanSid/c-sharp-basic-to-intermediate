7d9f890aa6ed901bbc5faa0cb57ab2b6d412328a
Saad ur rehman <51860160+SaadurrehmanSid@users.noreply.github.com>
2020-03-30 20:29:12 +0500

    Delegates in c#. Declaration of delegate

---
+++ Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
+
+
+public delegate void HelloFunctionDelegate(string strMessage);
+
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloFunctionDelegate hf = new HelloFunctionDelegate(Hello);
+            hf("this is a hello message");
+        }
+
+        public  static void Hello(string strMessage)
+        {
+            Console.WriteLine(strMessage);
+        }
+    }
+

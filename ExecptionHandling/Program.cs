730e29e36b183010fcc384c14189dec775ce0ec8
Saad ur rehman <51860160+SaadurrehmanSid@users.noreply.github.com>
2020-03-31 03:30:23 +0500

    Execption handling in c#

---
+++ ExecptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = new StreamReader(@"C:\Users\Saad ur rehman\Downloads\data.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.FileName);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally 
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Dispose();
+                }
+            }
+        }
+    }
+}

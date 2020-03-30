a90c1c8ca39f346036433aaaf136a49a1af791fd
Saad ur rehman <51860160+SaadurrehmanSid@users.noreply.github.com>
2020-03-30 18:21:13 +0500

    multiple class inheritance using interfaces

---
+++ MultiClassInhertitanceUsingInterfaces/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiClassInhertitanceUsingInterfaces
+{
+    interface IA {
+
+        void Aprint();
+    }
+    interface IB
+    {
+
+        void Bprint();
+    }
+
+    class A : IA
+    {
+        public void Aprint()
+        {
+            Console.WriteLine("CLass  A prit method");
+        }
+    }
+
+    class B : IB
+    {
+        public void Bprint()
+        {
+            Console.WriteLine("CLass B prit method");
+        }
+    }
+
+
+    class AB : IB,IA
+    {
+
+        IA a = new A();
+        IB b = new B();
+        public void Aprint()
+        {
+            a.Aprint();
+        }
+
+        public void Bprint()
+        {
+            b.Bprint();
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AB ab = new AB();
+
+            ab.Aprint();
+            ab.Bprint();
+        }
+    }
+}

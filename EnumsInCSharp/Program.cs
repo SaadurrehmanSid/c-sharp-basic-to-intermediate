3273118eae90dbf1cf35853f5b7587f017379a04
Saad ur rehman <51860160+SaadurrehmanSid@users.noreply.github.com>
2020-03-31 20:19:48 +0500

    enums in c sharp

---
+++ EnumsInCSharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employee = new List<Employee> 
+            {
+                new Employee{Name="Mark",Gender = Gender.male },
+                new Employee{ Name="Marry", Gender = Gender.female}
+            };
+
+            foreach (var emp in employee)
+            {
+                Console.WriteLine(emp.Name);
+                Console.WriteLine(emp.Gender);
+            }
+
+        }
+    }
+
+
+
+
+    public enum Gender 
+    {
+        unkown,
+        female,
+        male
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+    }
+}

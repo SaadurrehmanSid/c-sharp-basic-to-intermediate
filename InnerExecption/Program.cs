bf1d635f789d65c5d2b75ecd403961363080f44c
Saad ur rehman <51860160+SaadurrehmanSid@users.noreply.github.com>
2020-03-31 14:56:00 +0500

    Inner Execption

---
+++ InnerExecption/Program.cs
@@ -0,0 +1,37 @@
+﻿using Magnum.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerExecption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = null;
+
+            try
+            {
+                try
+                {
+                    streamReader = new StreamReader(@"C:\Users\Saad ur rehman\Downloadsssss\data.txt");
+                    Console.WriteLine(streamReader.ReadToEnd());
+                }
+                catch (Exception ex)
+                {
+                    throw new FileNotFoundException( " file is not present",ex);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("thrown execption "+exception.Message);
+                Console.WriteLine("Orignal Execption "+ exception.InnerException.Message);
+            }
+
+        }
+    }
+}

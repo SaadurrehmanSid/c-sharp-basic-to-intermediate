59724cede84bab82eb9408403783ed19b020be10
Saad ur rehman <51860160+SaadurrehmanSid@users.noreply.github.com>
2020-04-01 01:36:32 +0500

    Generics allow you to design classes and methods decoupled from data types

---
+++ GenericsInCShrap/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsInCShrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comparision<int> comparison = new Comparision<int>();
+            Console.WriteLine(comparison.match(5, 5));
+            
+        }
+    }
+
+
+   public  class Comparision<T>
+    {
+        public  bool match(T a, T b)
+        {
+            return a.Equals(b);
+        }
+    }
+}

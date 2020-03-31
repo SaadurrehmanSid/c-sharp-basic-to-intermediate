5f024742f0ddb0b57493055a21c2396b2c64c691
Saad ur rehman <51860160+SaadurrehmanSid@users.noreply.github.com>
2020-03-31 18:03:31 +0500

    Custom exception,how you can create your own exception

---
+++ CustomExecption/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CustomExecption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    throw new UserLoginExecption("the user cannot login twice on one device");
+                }
+                catch (UserLoginExecption ex)
+                {
+                    throw new Exception("this is the thrown exception",ex);
+                }
+            }
+            catch (Exception excp)
+            {
+                Console.WriteLine("thrown Exception" + excp.Message);
+                Console.WriteLine("Inner exception :"+ excp.InnerException.Message);
+            }
+
+        }
+    }
+
+
+    [Serializable]
+    class UserLoginExecption : Exception 
+    {
+        public UserLoginExecption() :base()
+        {
+        }
+
+        public UserLoginExecption(string message): base(message)
+        {
+        }
+
+        public UserLoginExecption(string message,Exception exception) : base(message,exception)
+        {
+        }
+
+        public UserLoginExecption(SerializationInfo info, StreamingContext
+              context) : base(info,context)
+        {
+        }
+    }
+}
